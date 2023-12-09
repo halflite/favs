9db359eecc66d33817092dcaa16989436da52076
halflite <shingo@halflite.net>
2023-12-09 10:25:25 +0000

    ソーシャルアイコン設定修正

--- src/config.ts
+++ src/config.ts
@@ -3,14 +3,14 @@ import type { Site, SocialObjects } from "./types";
 export const SITE: Site = {
   website: "https://astro-paper.pages.dev/", // replace this with your deployed domain
   author: "Sat Naing",
-  desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
-  title: "AstroPaper",
+  desc: "my favorites. witten by halflite.net",
+  title: "favorites... by halflite.net",
   ogImage: "astropaper-og.jpg",
   lightAndDarkMode: true,
   postPerPage: 3,
 };
 
-export const LOCALE = ["en-EN"]; // set to [] to use the environment default
+export const LOCALE = ["jp-JP"]; // set to [] to use the environment default
 
 export const LOGO_IMAGE = {
   enable: false,
@@ -21,39 +21,45 @@ export const LOGO_IMAGE = {
 
 export const SOCIALS: SocialObjects = [
   {
+    name: "Mail",
+    href: "mailto:shingo@halflite.net",
+    linkTitle: `Send an email to shingo@halflite.net`,
+    active: true,
+  },
+  {
+    name: "Mastodon",
+    href: "https://fedibird.com/@halflite",
+    linkTitle: `halflite@fedibird.com`,
+    active: true,
+  },
+  {
+    name: "Twitter",
+    href: "https://twitter.com/halflite",
+    linkTitle: `@halflite`,
+    active: true,
+  },
+  {
     name: "Github",
     href: "https://github.com/satnaing/astro-paper",
     linkTitle: ` ${SITE.title} on Github`,
-    active: true,
+    active: false,
   },
   {
     name: "Facebook",
     href: "https://github.com/satnaing/astro-paper",
     linkTitle: `${SITE.title} on Facebook`,
-    active: true,
+    active: false,
   },
   {
     name: "Instagram",
     href: "https://github.com/satnaing/astro-paper",
     linkTitle: `${SITE.title} on Instagram`,
-    active: true,
+    active: false,
   },
   {
     name: "LinkedIn",
     href: "https://github.com/satnaing/astro-paper",
     linkTitle: `${SITE.title} on LinkedIn`,
-    active: true,
-  },
-  {
-    name: "Mail",
-    href: "mailto:yourmail@gmail.com",
-    linkTitle: `Send an email to ${SITE.title}`,
-    active: false,
-  },
-  {
-    name: "Twitter",
-    href: "https://github.com/satnaing/astro-paper",
-    linkTitle: `${SITE.title} on Twitter`,
     active: false,
   },
   {
@@ -132,12 +138,6 @@ export const SOCIALS: SocialObjects = [
     name: "Telegram",
     href: "https://github.com/satnaing/astro-paper",
     linkTitle: `${SITE.title} on Telegram`,
-    active: false,
-  },
-  {
-    name: "Mastodon",
-    href: "https://github.com/satnaing/astro-paper",
-    linkTitle: `${SITE.title} on Mastodon`,
     active: false,
   },
 ];
