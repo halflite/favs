e9b2002fde16cc0cf05cafd351845c73d9f2cafe
halflite <shingo@halflite.net>
2023-12-24 05:54:31 +0000

    見た目のカスタマイズ

--- src/utils/generateOgImages.tsx
+++ src/utils/generateOgImages.tsx
@@ -1,40 +1,28 @@
 import satori, { type SatoriOptions } from "satori";
+import fs from "fs/promises";
 import { Resvg } from "@resvg/resvg-js";
 import { type CollectionEntry } from "astro:content";
 import postOgImage from "./og-templates/post";
 import siteOgImage from "./og-templates/site";
 
-const fetchFonts = async () => {
-  // Regular Font
-  const fontFileRegular = await fetch(
-    "https://www.1001fonts.com/download/font/ibm-plex-mono.regular.ttf"
-  );
-  const fontRegular: ArrayBuffer = await fontFileRegular.arrayBuffer();
-
-  // Bold Font
-  const fontFileBold = await fetch(
-    "https://www.1001fonts.com/download/font/ibm-plex-mono.bold.ttf"
-  );
-  const fontBold: ArrayBuffer = await fontFileBold.arrayBuffer();
-
-  return { fontRegular, fontBold };
-};
-
-const { fontRegular, fontBold } = await fetchFonts();
+const [ fontRegular, fontBold ] = await Promise.all([
+  fs.readFile("./public/fonts/BIZUDPGothic-Regular.ttf"),
+  fs.readFile("./public/fonts/BIZUDPGothic-Bold.ttf"),
+]);
 
 const options: SatoriOptions = {
   width: 1200,
   height: 630,
-  embedFont: true,
+  embedFont: false,
   fonts: [
     {
-      name: "IBM Plex Mono",
+      name: "Zen Kaku Gothic New",
       data: fontRegular,
       weight: 400,
       style: "normal",
     },
     {
-      name: "IBM Plex Mono",
+      name: "Zen Kaku Gothic New",
       data: fontBold,
       weight: 600,
       style: "normal",
